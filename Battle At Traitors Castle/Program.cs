1ea6a5be719ff3884645f64ef2f9aa9dc2154eeb
Michael Knowles <michael.knowles@tba.group>
2020-11-27 13:31:08 +0000

    Removed code smells Better string building

--- Battle At Traitors Castle/Program.cs
+++ Battle At Traitors Castle/Program.cs
@@ -1,42 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Battle_At_Traitor_s_Castle
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.Title = "TRAITOR'S CASTLE";
             int shots = 0;
 
-            for(int attempt=0; attempt < 10; attempt++)
+            Random rnd = new Random();
+            for (int attempt=0; attempt < 10; attempt++)
             {
-                Random rnd = new Random();
                 int guardPosition = rnd.Next(1, 10);
-                string row = "";
-                for(int i=1; i < 10; i++)
-                {
-                    if (guardPosition == i)
-                    {
-                        row += "O";
-                    }
-                    else
-                    {
-                        row += ".";
-                    }
-                }
+                string row = "........";
+                row = row.Insert(guardPosition - 1, "O");
                 ClearKeyPresses();
                 Console.WriteLine(row);
                 Thread.Sleep(2000);
                 if (Console.KeyAvailable)
                 {
-                    int keyPressed;
-                    int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out keyPressed);
+                    int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out int keyPressed);
                     if (keyPressed == guardPosition)
                     {
                         Console.WriteLine("GOOD SHOT");
@@ -47,7 +32,7 @@ namespace Battle_At_Traitor_s_Castle
                 Console.WriteLine("MISSED");
             }
 
-            Console.WriteLine("YOU HIT " + shots.ToString() + " OUT OF 10");
+            Console.WriteLine($"YOU HIT {shots} OUT OF 10");
             Console.ReadKey();
         }
 
