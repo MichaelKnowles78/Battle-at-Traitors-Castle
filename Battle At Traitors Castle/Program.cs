69a4574b0f918053a120eb3cf726929134342cbb
Michael Knowles <39923992+MichaelKnowles78@users.noreply.github.com>
2020-11-27 14:12:07 +0000

    Code clean (#1)
    * Removed code smells
    Better string building
    
    * Added "Play again" functionality

--- Battle At Traitors Castle/Program.cs
+++ Battle At Traitors Castle/Program.cs
@@ -1,42 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Battle_At_Traitor_s_Castle
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Entry point.  Initialise, play, ask to play again
+        /// </summary>
+        static void Main()
         {
             Console.Title = "TRAITOR'S CASTLE";
-            int shots = 0;
 
-            for(int attempt=0; attempt < 10; attempt++)
+            do
             {
-                Random rnd = new Random();
+                GameLoop();
+            } while (PlayAgain());
+        }
+
+        /// <summary>
+        /// Main game loop
+        /// </summary>
+        private static void GameLoop()
+        {
+            Console.Clear();
+            int shots = 0;
+            Random rnd = new Random();
+            for (int attempt = 0; attempt < 10; attempt++)
+            {
                 int guardPosition = rnd.Next(1, 10);
-                string row = "";
-                for(int i=1; i < 10; i++)
-                {
-                    if (guardPosition == i)
-                    {
-                        row += "O";
-                    }
-                    else
-                    {
-                        row += ".";
-                    }
-                }
+                string row = "........";
+                row = row.Insert(guardPosition - 1, "O");
                 ClearKeyPresses();
                 Console.WriteLine(row);
                 Thread.Sleep(2000);
                 if (Console.KeyAvailable)
                 {
-                    int keyPressed;
-                    int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out keyPressed);
+                    int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out int keyPressed);
                     if (keyPressed == guardPosition)
                     {
                         Console.WriteLine("GOOD SHOT");
@@ -47,16 +47,35 @@ namespace Battle_At_Traitor_s_Castle
                 Console.WriteLine("MISSED");
             }
 
-            Console.WriteLine("YOU HIT " + shots.ToString() + " OUT OF 10");
-            Console.ReadKey();
+            Console.WriteLine($"YOU HIT {shots} OUT OF 10");
         }
 
+        /// <summary>
+        /// Will clear keyboard buffer to prevent too many keystrokes affecting the next shot
+        /// </summary>
         static void ClearKeyPresses()
         {
             while(Console.KeyAvailable)
             {
                 Console.ReadKey(true);
             }
+        }
+
+        /// <summary>        
+        /// Will ask if user wants to play again and will wait until either "Y" or "N" is pressed. Can handle both upper and lower case
+        /// </summary>
+        /// <returns>Returns true if "Y" is pressed or false if "N" is pressed.</returns>
+        private static bool PlayAgain()
+        {
+            Console.WriteLine("PLAY AGAIN (Y/N)");
+
+            char keyPressed;
+            do
+            {
+                keyPressed = Console.ReadKey(true).KeyChar;
+            } while (!"yYnN".Contains(keyPressed.ToString()));
+
+            return keyPressed == 'y' || keyPressed == 'Y';
         }
     }
 }
