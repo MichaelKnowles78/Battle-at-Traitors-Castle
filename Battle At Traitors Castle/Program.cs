19e274443b273dcd62a4618db16756df42ee634e
Michael Knowles <michael.knowles@tba.group>
2020-11-27 13:59:59 +0000

    Added "Play again" functionality

--- Battle At Traitors Castle/Program.cs
+++ Battle At Traitors Castle/Program.cs
@@ -5,13 +5,28 @@ namespace Battle_At_Traitor_s_Castle
 {
     static class Program
     {
+        /// <summary>
+        /// Entry point.  Initialise, play, ask to play again
+        /// </summary>
         static void Main()
         {
             Console.Title = "TRAITOR'S CASTLE";
-            int shots = 0;
 
+            do
+            {
+                GameLoop();
+            } while (PlayAgain());
+        }
+
+        /// <summary>
+        /// Main game loop
+        /// </summary>
+        private static void GameLoop()
+        {
+            Console.Clear();
+            int shots = 0;
             Random rnd = new Random();
-            for (int attempt=0; attempt < 10; attempt++)
+            for (int attempt = 0; attempt < 10; attempt++)
             {
                 int guardPosition = rnd.Next(1, 10);
                 string row = "........";
@@ -33,15 +48,34 @@ namespace Battle_At_Traitor_s_Castle
             }
 
             Console.WriteLine($"YOU HIT {shots} OUT OF 10");
-            Console.ReadKey();
         }
 
+        /// <summary>
+        /// Will clear keyboard buffer to prevent too many keystrokes affecting the next shot
+        /// </summary>
         static void ClearKeyPresses()
         {
             while(Console.KeyAvailable)
             {
                 Console.ReadKey(true);
             }
+        }
+
+        /// <summary>        
+        /// Will ask if user wants to play again and will wait until either "Y" or "N" is pressed. Can handle both upper and lower case
+        /// </summary>
+        /// <returns>Returns true if "Y" is pressed or false if "N" is pressed.</returns>
+        private static bool PlayAgain()
+        {
+            Console.WriteLine("PLAY AGAIN (Y/N)");
+
+            char keyPressed;
+            do
+            {
+                keyPressed = Console.ReadKey(true).KeyChar;
+            } while (!"yYnN".Contains(keyPressed.ToString()));
+
+            return keyPressed == 'y' || keyPressed == 'Y';
         }
     }
 }
